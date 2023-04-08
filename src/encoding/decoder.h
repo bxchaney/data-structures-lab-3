e59d8be17074c98316dfa60b1091ed100dc721e0
bxchaney <bxchaney@gmail.com>
2023-04-08 17:17:22 -0400

    inital commit

---
+++ src/encoding/decoder.h
@@ -0,0 +1,9 @@
+#pragma once
+#include<iostream>
+
+class Decoder
+{
+    public:
+        Decoder();
+        ~Decoder();
+};
---
+++ src/encoding/encoder.h
@@ -0,0 +1,9 @@
+#pragma once
+#include<iostream>
+
+class Encoder
+{
+    public:
+        Encoder();
+        ~Encoder();
+};
