a7d7d12ce4f59a2f5ddb164753e63b4790c57e11
bxchaney <bxchaney@gmail.com>
2023-04-09 12:24:10 -0400

    beginning declarations

--- src/encoding/utils/huffman.h
+++ src/encoding/utils/huffman.h
@@ -1,9 +1,30 @@
 #pragma once
+#include<memory>
+#include<string>
+#include"huff_tree.h"
+#include"freq_table.h"
 
-class Huffman {
+
+
+class Huffman 
+{
+    private:
+        std::unique_ptr<HuffTree[]> huff_heap;
+        int _heap_size;        
+        int _heap_max_size;
+
+        void percolate_down();
+        void percolate_up();
+        HuffTree& pop();
+        void push(HuffTree&);
 
     public:
-        Huffman();
+        Huffman() = default;
+        Huffman(FrequencyTable);
         ~Huffman();
         
+        bool is_character(std::string);
+        std::string get_character(std::string);
+
+        std::string get_code(std::string);
 };
