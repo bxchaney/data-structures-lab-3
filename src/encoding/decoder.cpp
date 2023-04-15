42a2c5505cd20684ec5bda8cd3125f02b0554ca5
bxchaney <bxchaney@gmail.com>
2023-04-15 14:31:05 -0400

    inital commit

--- src/encoding/decoder.cpp
+++ src/encoding/decoder.cpp
@@ -1,1 +1,55 @@
+#include<iostream>
+#include<string>
+#include<fstream>
 #include"decoder.h"
+#include"utils/freq_table.h"
+#include"utils/huffman.h"
+#include"utils/code_table.h"
+
+Decoder::Decoder(std::istream& freq_table)
+{
+    ft = FrequencyTable{freq_table};
+    huff.from_freq(ft);
+}
+
+void Decoder::decode(std::istream& is)
+{
+    // we assume that the encoded message is only 0's, 1's, and newlines
+    std::string s;
+    char c;
+    while ((c = is.get()) != EOF)
+    {
+        if (c == '\n')
+        {
+            if( s != "" && s != "\r") { std::cout << "uh oh: |" << s << "|"<< std::endl;}
+            std::cout << std::endl;
+            decoded_message << std::endl;
+            s = "";
+            continue;
+        }
+        s = s + c;
+        if (huff.is_character_encoding(s))
+        {
+            std::string x = huff.get_character(s);
+            std::cout << x;
+            decoded_message << x;
+            s = "";
+        }
+    }
+}
+
+FrequencyTable& Decoder::get_freq_table()
+{
+    return ft;
+}
+
+EncodingTable Decoder::get_code_table()
+{
+    return huff.get_encoding_table();
+}
+
+std::ostream& operator<<(std::ostream& os, Decoder& dec)
+{
+    os << dec.decoded_message.rdbuf();
+    return os;
+}
--- src/encoding/decoder.h
+++ src/encoding/decoder.h
@@ -1,9 +1,33 @@
 #pragma once
 #include<iostream>
+#include<fstream>
+#include<string>
+#include<memory>
+#include"utils/freq_table.h"
+#include"utils/huffman.h"
+#include"utils/code_table.h"
 
 class Decoder
 {
+    private:
+
+        FrequencyTable ft;
+        Huffman huff;
+
+        std::filebuf buffer;
+        std::ostream decoded_message {&buffer};
+
     public:
-        Decoder();
-        ~Decoder();
+        Decoder() = default;
+        Decoder(std::istream&);
+        ~Decoder() = default;
+
+        void decode(std::istream&);
+
+        FrequencyTable& get_freq_table();
+
+        EncodingTable get_code_table();
+
+        friend std::ostream& operator<<(std::ostream&, Decoder&);
+
 };
