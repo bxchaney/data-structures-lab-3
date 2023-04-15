23fda07e3c292c84741aa66c38f3bb00164c491b
bxchaney <bxchaney@gmail.com>
2023-04-15 18:51:27 -0400

    inital commit

--- src/encoding/encoder.cpp
+++ src/encoding/encoder.cpp
@@ -1,1 +1,117 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+#include"utils/freq_table.h"
+#include"utils/tree_map.h"
+#include"utils/huffman.h"
 #include"encoder.h"
+
+Encoder::Encoder(FrequencyTable& ft)
+{
+    this->ft = ft;
+    _freq_table_provided = true;
+    huff.from_freq(ft);
+}
+
+void Encoder::encode(std::istream& is)
+{
+    if (!_freq_table_provided)
+    {
+        make_encoding(is);
+        write_encoding();
+    }
+    write_encoding(is);
+}
+
+void Encoder::make_encoding(std::istream& is)
+{
+    std::string s;
+    while (!is.eof())
+    {
+        s = is.get();
+        if (s == "\n")
+        {
+            s_stream << std::endl;
+        }
+        else if (s == "\r")
+        {
+            continue;
+        }
+        else
+        {
+            tree_map[s]++;
+            s_stream << s;
+        }
+    }
+    tree_map.fill_freq_table(ft);
+    huff.from_freq(ft);
+}
+
+void Encoder::write_encoding()
+{
+    // case when freq table is not provided
+    std::string s;
+    while (s_stream)
+    {
+        s_stream >> s;
+        if (s == "\n")
+        {
+            encoded_message << std::endl;
+        }
+        else if (s == "\r")
+        {
+            continue;
+        }
+        else 
+        {
+            encoded_message << huff.get_code(s);
+        }
+    }
+}
+
+void Encoder::write_encoding(std::istream& is)
+{
+    char c;
+    std::string s;
+    while (!is.eof())
+    {
+        c = is.get();
+        if (c == '\n')
+        {
+            encoded_message << std::endl;
+        }
+        // ignoring punctuation
+        else if (
+            c == '\r' 
+            || c == '.'
+            || c == '!'
+            || c == ','
+            || c == '\''
+            || c == '?'
+            || c == ' '
+            || c == '-'
+            )
+        {
+            continue;
+        }
+        else if 
+            (
+                c >= 'a' && c <= 'z'
+            )
+        {
+            // converting to uppercase
+            s = c + 32;
+        }
+        else 
+        {
+            s = c;
+        }
+        encoded_message << huff.get_code(s);
+    }
+}
+
+std::ostream& operator<<(std::ostream& os, Encoder& en)
+{
+    os << en.encoded_message.rdbuf();
+    return os;
+}
--- src/encoding/encoder.h
+++ src/encoding/encoder.h
@@ -1,9 +1,36 @@
 #pragma once
 #include<iostream>
+#include<fstream>
+#include<string>
+#include<sstream>
+#include"utils/huffman.h"
+#include"utils/freq_table.h"
+#include"utils/tree_map.h"
 
 class Encoder
 {
+    private:
+        FrequencyTable ft;
+        Huffman huff;
+        bool _freq_table_provided;
+        TreeMap tree_map;
+
+        std::filebuf encoding_buffer;
+        std::ostream encoded_message {&encoding_buffer};
+
+        std::stringstream s_stream;
+
+        void write_encoding();
+        void write_encoding(std::istream&);
+        void make_encoding(std::istream&);
+
     public:
-        Encoder();
-        ~Encoder();
+        Encoder() = default;
+        Encoder(FrequencyTable&);
+
+        ~Encoder() = default;
+
+        void encode(std::istream&);
+
+        friend std::ostream& operator<<(std::ostream&, Encoder&);
 };
